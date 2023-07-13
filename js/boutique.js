e75871b02096a91a04163b0819fde535ad9d0fea
mikl291 <tyka2923@gmail.com>
2023-07-13 16:01:49 +0100

    Update card project

--- js/boutique.js
+++ js/boutique.js
@@ -1,21 +1,17 @@
 $(document).ready(function() {
-  $.getJSON("vcard.json", function(data) {
-    var images = data.images;
-
-    var carousel = $(".carousel");
-    images.forEach(function(image) {
-      var slide = $("<div>").addClass("carousel-slide");
-      var img = $("<img>").attr("src", image).appendTo(slide);
-      slide.appendTo(carousel);
-    });
-
-    carousel.slick({
-      dots: false,
-      arrows: false,
-      slidesToShow: images.length, 
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 1000
-    });
+  $(".carousel").owlCarousel({
+    items: 3,
+    loop: true,
+    margin: 20,
+    nav: false,
+    dots: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      768: {
+        items: 3
+      }
+    }
   });
 });
