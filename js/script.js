a902be7a08668af58fb068d5ace477be9042652d
tyka29 <121292211+tyka29@users.noreply.github.com>
2023-07-17 16:37:14 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,9 +9,9 @@ fetch('db.json')
       projectThumbnail.classList.add('project-thumbnail');
       projectThumbnail.innerHTML = `
         <img src="${project.image}" alt="${project.alt}">
-        <h3>${project.title}</h3>
+        <h3><span>${project.title}</span></h3>
         <p>${project.description}</p>
-        <p><strong>Compétences :</strong> ${project.competences}</p>
+        <span><strong>Compétences</span> :</strong> ${project.competences}
         <a href="${project.link}">En savoir plus</a>
       `;
       projectsContainer.appendChild(projectThumbnail);
@@ -20,23 +20,3 @@ fetch('db.json')
   .catch(error => {
     console.error('Erreur lors du chargement des données db.json :', error);
   });
-
-// Gestion du changement d'image pour le carrousel du téléphone
-window.addEventListener('DOMContentLoaded', function() {
-  var phoneImages = document.querySelectorAll('.phone .contact-img');
-  var currentIndex = 0;
-  var interval = 10000; // Délai de 10 secondes en millisecondes
-
-  function changeImage() {
-    phoneImages.forEach(function(image) {
-      image.classList.add('hidden');
-    });
-
-    currentIndex = (currentIndex + 1) % phoneImages.length;
-
-    phoneImages[currentIndex].classList.remove('hidden');
-  }
-
-  // Exécute la fonction de changement d'image toutes les 10 secondes
-  setInterval(changeImage, interval);
-});
